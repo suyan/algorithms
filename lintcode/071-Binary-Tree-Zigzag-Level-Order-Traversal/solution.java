910112f4ac3acfb5d9652d9f4b154161b9c4f321
Su Yan <suyan0830@gmail.com>
2016-05-30 16:25:47 -0700

    solved zigzag level order traversal

---
+++ lintcode/071-Binary-Tree-Zigzag-Level-Order-Traversal/solution.java
@@ -0,0 +1,52 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+ 
+ 
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: A list of lists of integer include 
+     *          the zigzag level order traversal of its nodes' values 
+     */
+    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        boolean reverse = false;
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            for (int i = 0; i < len; i++) {
+                TreeNode curr = queue.poll();
+                level.add(curr.val);
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+            if (reverse == true) {
+                Collections.reverse(level);
+            }
+            reverse = !reverse;
+            result.add(level);
+        }
+        
+        return result;
+    }
+}
