7f46f04547596f177b709e9d25b3e0d9f86cfd09
Su Yan <suyan0830@gmail.com>
2016-08-09 23:28:10 -0700

    solved triangle problem

--- lintcode/109-Triangle/solution.java
+++ lintcode/109-Triangle/solution.java
@@ -4,6 +4,27 @@ public class Solution {
      * @return: An integer, minimum path sum.
      */
     public int minimumTotal(int[][] triangle) {
-        // write your code here
+        if (triangle == null || triangle.length == 0) {
+            return 0;
+        }
+        
+        int len = triangle.length;
+        
+        int[] dp = new int[len];
+        for (int i = 0; i < triangle[len - 1].length; i++) {
+            dp[i] = triangle[len - 1][i];
+        }
+
+        for (int i = len - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                if (dp[j] < dp[j + 1]) {
+                    dp[j] = triangle[i][j] + dp[j];
+                } else {
+                    dp[j] = triangle[i][j] + dp[j + 1];
+                }
+            }
+        }
+
+        return dp[0];
     }
 }
